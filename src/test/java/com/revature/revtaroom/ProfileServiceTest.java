9797ccd2285636dd989fdead551d909fda1e4edc
Nicolo Perrelli <nicoloperrelli@gmail.com>
2019-10-17 23:43:13 -0400

    pulling

---
+++ src/test/java/com/revature/revtaroom/ProfileServiceTest.java
@@ -0,0 +1,8 @@
+package com.revature.revtaroom;
+
+import com.revature.services.ProfileService;
+
+public class ProfileServiceTest {
+
+	private ProfileService profileService = new ProfileService(null);
+}
